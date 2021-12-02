c11b960b93563ab7ffb85af1d6053f35efc8a578
Joe <joebreilly@yahoo.ie>
2021-12-02 12:36:01 +0000

    User authentication added to add_review view. Delete review functionality added to review app view and url

--- reviews/views.py
+++ reviews/views.py
@@ -4,9 +4,11 @@ from .forms import Add_ReviewForm
 from django.contrib import messages
 from products.models import Product
 from profiles.models import UserProfile
+from django.contrib.auth.decorators import login_required
 from django.urls import reverse
 
 
+@login_required
 def add_review(request, product_id):
     ''' View to add reviews '''
     product = get_object_or_404(Product, pk=product_id)
@@ -19,9 +21,28 @@ def add_review(request, product_id):
             review.save()
             messages.success(request, 'Successfully added review!')
             return redirect(reverse('product_detail', args=[product.id]))
-    form = Add_ReviewForm()
+        else:
+            messages.error(request, 'Failed to add review. \
+                Please ensure all form fields are valid')
+    else:
+        form = Add_ReviewForm()
+    template = 'reviews/add_review.html'
     context = {
         'form': form,
         'product': product,
     }
-    return render(request, 'reviews/add_review.html', context)
+    return render(request, template, context)
+
+
+@login_required()
+def delete_review(request, product_id, review_id):
+    '''Delete a product from the store'''
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry only store owners can do that!')
+        return redirect(reverse('home'))
+        
+    product = get_object_or_404(Product, pk=product_id)
+    review = Review.objects.get(product=product, pk=review_id)
+    review.delete()
+    messages.success(request, 'Review deleted!')
+    return redirect(reverse('product_detail', args=[product.id]))
