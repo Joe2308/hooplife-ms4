7d24207bb8c81d733fec46e89fb413e04aa8e3fc
Joe <joebreilly@yahoo.ie>
2021-12-04 12:46:25 +0000

    View created to add products to user's wishlist by product id, url created in wishlist app. Add to wishlist button to trigger add_to_wishlist view added to product details page

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404, HttpResponse
+from django.shortcuts import render, reverse, redirect, HttpResponse, get_object_or_404
 from products.models import Product
 from profiles.models import UserProfile
 from wishlist.models import Wishlist, WishlistItem
+from django.contrib import messages
 
 
 def wishlist(request):
@@ -20,3 +21,22 @@ def wishlist(request):
     }
 
     return render(request, 'wishlist/wishlist.html', context=context)
+
+
+def add_to_wishlist(request, product_id):
+    """
+    A view to a product from the store to the user's
+    wishlist
+    """
+    product = get_object_or_404(Product, pk=product_id)
+    profile = get_object_or_404(UserProfile, user=request.user)
+
+    wishlist, created = Wishlist.objects.get_or_create(user=profile)
+
+    if WishlistItem.objects.filter(product=product).exists():
+        messages.error(request,'Product already in your wishlist!')
+        return redirect(reverse('product_detail', args=[product.id]))
+    else:
+        wishlist.products.add(product)
+        messages.success(request, 'product added to wishlist!')
+        return redirect(reverse('product_detail', args=[product.id]))
