978cdeacb70cf328dbc00c5b8bdef8f04ebc0079
Joe <joebreilly@yahoo.ie>
2021-12-04 11:16:00 +0000

    wishlist app and model created, migrations ran for rating field change in review model

---
+++ wishlist/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from profiles.models import UserProfile
+from products.models import Product
+
+
+class Wishlist(models.Model):
+    user = models.OneToOneField(
+        UserProfile, null=False, blank=False,
+        on_delete=models.CASCADE, related_name='wishlist'
+        )
+    products = models.ManyToManyField(
+        Product, default="", through='WishlistItem'
+        )
+
+    def __str__(self):
+        return f'Wishlist ({self.user})'
+
+
+class WishlistItem(models.Model):
+    wishlist = models.ForeignKey(
+        Wishlist, null=False, blank=False, on_delete=models.CASCADE,
+        related_name='wishlist_items')
+    product = models.ForeignKey(
+        Product, null=False, blank=False, on_delete=models.CASCADE,
+        related_name='wishlist_products')
+
+    def __str__(self):
+        return self.product.name
