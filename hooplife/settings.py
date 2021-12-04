39f8975f176c10611e4f0ff3af2e3cad2f31c531
Joe <joebreilly@yahoo.ie>
2021-12-04 16:51:27 +0000

    Product data dumped to json file and loaded to Heroku postgres database

--- hooplife/settings.py
+++ hooplife/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+# import dj_database_url
 
 if os.path.exists("env.py"):
     import env
@@ -124,6 +125,7 @@ WSGI_APPLICATION = 'hooplife.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
 
 DATABASES = {
     'default': {
