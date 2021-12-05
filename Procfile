209e7b249d86f0e504e2095b0e4c6cc14390a829
Joe <joebreilly@yahoo.ie>
2021-12-05 08:47:55 +0000

    Installed gunicorn and disable static files in heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn hooplife.wsgi:application
--- hooplife/settings.py
+++ hooplife/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-# import dj_database_url
+import dj_database_url
 
 if os.path.exists("env.py"):
     import env
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ms4-hooplife.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -127,13 +127,17 @@ WSGI_APPLICATION = 'hooplife.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
